98a745ea45ecfe5ee0360b1208904708a584a398
Ni Chia <nichia@gmail.com>
2017-11-03 01:41:29 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,45 @@
+#properties
+#  has a title (FAILED - 2)
+#  has an author name (FAILED - 3)
+#  has a page count (FAILED - 4)
+#  has a genre (FAILED - 5)
+#turn_page
+#  can turn the page (FAILED - 6)
 
+class Book
+  def initialize(title)
+    @title = title
+  end
 
+  def title
+    @title
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+  def page_count=(page_count)
+    @page_count = page_count
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
